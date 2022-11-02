492555f3a01acae51c24b7d8d951913380d6c2b0
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-02 17:53:44 -0300

    modificacion de tipo de data

--- src/dataHotels.js
+++ src/dataHotels.js
@@ -1,90 +1,90 @@
-[
+let dataHotels = [
     {
-     "id": "ho1",
+     id: "ho1",
      "name": "St. Regis Almasa Hotel",
      "photo": ["https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/2b/89/6b/hotel-enterance.jpg?w=700&h=-1&s=1",
       "https://cf.bstatic.com/images/hotel/840x460/363/363705857.jpg",
       "https://s3.amazonaws.com/blackbooktravels/inventory/hotels/images/ddbc8a5096d1ec819b7863f1b5dcc7051a27445131afe1c80134a6f1d319b1695cf394.jpeg"
      ],
-     "capacity": "600",
+     "capacity": 600,
      "description": "It is one of the most luxurious hotels in Cairo",
      "cityId": "city1",
      "userId": "adm1"
      },
       {
-     "id": "ho2",
+     id: "ho2",
      "name": "Athens Cypria Hotel",
      "photo": ["https://media.staticontent.com/media/pictures/19884b41-8bf0-4008-b90c-145708089ac7",
       "https://athenscypria.com/wp-content/uploads/2020/03/slider-2.jpg",
       "https://pix10.agoda.net/hotelImages/506/50697/50697_16082319240045742113.jpg?ca=6&ce=1&s=1024x768"
      ],
-     "capacity": "300",
+     "capacity": 300,
      "description": "Just 300 meters from Syntagma Metro Station, Athens Cypria Hotel features a stylish café-bar, internet corner and a rooftop bar with views of the Acropolis",
      "cityId": "city2",
      "userId": "adm2"
      },
      {
-     "id": "ho3",
+     id: "ho3",
      "name": "Novotel Paris Les Halles",
      "photo": ["https://www.novotelparis.com/photos-2/ourhotel-1.jpg",
       "https://assets.hotelaparis.com/uploads/pictures/000/043/237/novotel-paris-les-halles-11-HD.jpg",
       "https://assets.hotelaparis.com/uploads/pictures/000/043/236/_N2Q8098.jpg"
      ],
-     "capacity": "500",
+     "capacity": 500,
      "description": "Known for its family-friendly environment and proximity to great restaurants and attractions, Novotel Paris Les Halles makes it easy to enjoy the best of Paris",
      "cityId": "city3",
      "userId": "adm3"
      },
      {
-     "id": "ho4",
+     id: "ho4",
      "name": "Westin Rusutsu Resort",
      "photo": ["https://rusutsu.com/wp-content/uploads/westin-main_sp.jpg",
       "https://cache.marriott.com/content/dam/marriott-renditions/CTSWI/ctswi-lobby-morning-4953-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1336px:*",
       "https://pix10.agoda.net/hotelImages/114/1147341/1147341_17081811260055498515.jpg?ca=6&ce=1&s=1024x768"
      ],
-     "capacity": "400",
+     "capacity": 400,
      "description": "Located 400 meters from Rusutsu Ski Resort, The Westin Rusutsu Resort offers rooms with air conditioning in Rusutsu. The property is within walking distance of several attractions, including the West Gondola and the West 2nd Quad",
      "cityId": "city4",
      "userId": "adm4"
      },
      {
-     "id": "ho5",
+     id: "ho5",
      "name": "InterContinental Wellington",
      "photo": ["https://digital.ihg.com/is/image/ihg/intercontinental-wellington-6002915753-2x1",
       "https://digital.ihg.com/is/image/ihg/intercontinental-wellington-6002934439-2x1",
       "https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/http://www.cfmedia.vfmleonardo.com/imageRepo/7/0/109/541/105/WLGGS_6002926884_O/InterContinental-Wellington-Pool.jpg?tr=w-780%2Ch-437%2Cfo-auto"
      ],
-     "capacity": "600",
+     "capacity": 600,
      "description": "The 5-star InterContinental Wellington is located next to the harbor and just 5 miles from Wellington International Airport",
      "cityId": "city5",
      "userId": "adm1"
      },
      {
-     "id": "ho6",
+     id: "ho6",
      "name": "Las Hayas Ushuaia Resort",
      "photo": ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/227940923.jpg?k=c20e08670c404d7b7db6b24ead218227839dfd0e2bc4e485633aaf6c38530891&o=&hp=1",
       "https://q-xx.bstatic.com/xdata/images/hotel/840x460/366244014.jpg?k=2be23b3adec0302bfb6e8dcdc8f80565e8748b00d5d19743a3515009736b720c&o=",
       "https://cf.bstatic.com/images/hotel/840x460/263/263239551.jpg"
      ],
-     "capacity": "300",
+     "capacity": 300,
      "description": "Las Hayas Ushuaia Resort offers comfortable accommodation with a spa, indoor pool and hot tub. The property has a restaurant, terrace and free Wi-Fi in public areas. Ushuaia Port is 3 km away",
      "cityId": "city6",
      "userId": "adm2"
      },
      {
-     "id": "ho7",
+     id: "ho7",
      "name": "Sofitel Legend The Grand Amsterdam",
      "photo": ["https://i0.wp.com/www.menstylefashion.com/wp-content/uploads/2016/08/Sofitel-legend-the-grand-amsterdam-entrance.jpg?resize=1184%2C640&ssl=1",
       "https://www.sofitel-legend-thegrand.com/_novaimg/galleria/1450281.jpg",
       "https://www.sofitel-legend-thegrand.com/_novaimg/galleria/1426434.jpg"
      ],
-     "capacity": "500",
+     "capacity": 500,
      "description": "Located in the heart of Amsterdam, this 5-star hotel offers luxurious rooms in an exclusive historic setting with French elegance",
      "cityId": "city7",
      "userId": "adm3"
      },
      {
-     "id": "ho8",
+     id: "ho8",
      "name": "Grand Hotel Bohemia",
      "photo": ["https://ak-d.tripcdn.com/images/220o16000001087iz4F6E_Z_1100_824_R5_Q70_D.jpg",
       "https://pix10.agoda.net/hotelImages/285/2858/2858_15060216040028254938.jpg?ca=4&ce=1&s=1024x768",
@@ -96,49 +96,49 @@
      "userId": "adm4"
      },
      {
-     "id": "ho9",
+     id: "ho9",
      "name": "Artesana Luxury Villa",
      "photo": ["https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1d/f0/70/ca/artesana-luxury-villa.jpg?w=700&h=-1&s=1",
       "https://ak-d.tripcdn.com/images/02256120009tl343g7C99_R_800_525.jpg",
       "https://cf.bstatic.com/images/hotel/840x460/380/380150978.jpg"
      ],
-     "capacity": "550",
+     "capacity": 550,
      "description": "This property has a 24-hour reception and offers a bicycle rental service. La Artesana Luxury Villa is attached to a 5-star hotel and guests have access to its swimming pool, hot tub, gym and spa center",
      "cityId": "city9",
      "userId": "adm1"
      },
      {
-     "id": "ho10",
+     id: "ho10",
      "name": "Avani Atrium Bangkok Hotel",
      "photo": ["https://pix10.agoda.net/hotelImages/23112526/0/628a73f3d71a49a8114039c9bffb40c5.jpg?ca=17&ce=1&s=1024x768",
       "https://cf.bstatic.com/xdata/images/hotel/max1024x768/163279019.jpg?k=4f2ff0a4c17a0b85701bf4be3ba5f08f5d5de7bcb1549cf3595a9c3646fb7641&o=&hp=1",
       "https://r1imghtlak.mmtcdn.com/7bf49142256411ea81150242ac110004.jpg?&output-quality=75&downsize=910:612&crop=910:612;16,0&output-format=jpg"
      ],
-     "capacity": "700",
+     "capacity": 700,
      "description": "Avani Atrium Bangkok is located along New Petchburi Road, 700 metres from Phetchaburi MRT Station and hotel also provides drop off service to Phetchaburi MRT at exit 3 as well. Boasting a swimming pool, fitness centre and free WiFi in all areas, this hotel offers chargeable limousine services",
      "cityId": "city10",
      "userId": "adm2"
      },
      {
-     "id": "ho11",
+     id: "ho11",
      "name": "La Concha Best View",
      "photo": ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/332285289.jpg?k=4706b6187ddb8bc9cf1b33ea6780eb3d7c0f1b2450133b05a7bce73931ad6179&o=&hp=1",
       "https://cf.bstatic.com/xdata/images/hotel/max1024x768/332285290.jpg?k=7764874355a9e882e89df7ff086c6b10f7a01338cf0db70f44a8f20709243fab&o=&hp=1",
       "https://cf.bstatic.com/xdata/images/hotel/max1024x768/332285325.jpg?k=b4f36b360a495e2fd13b7a4d5a9424c4f22cf11c998c8af07efcf705955e6be3&o=&hp=1"
      ],
-     "capacity": "400",
+     "capacity": 400,
      "description": "Set in San Sebastián, just 50 meters from La Concha Beach, La Concha Best View offers beachfront accommodation with a terrace and free WiFi. This apartment is 1.2 km from Victoria Eugenia Theater and 1.7 km from Calle Mayor",
      "cityId": "city11",
      "userId": "adm3"
      },
      {
-     "id": "ho12",
+     id: "ho12",
      "name": "Ouarzazate Hotel",
      "photo": ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/95421013.jpg?k=dd3b5cc501da28b03b201a5dee28d93c0da963949393bd357fd73d6a23359a4a&o=&hp=1",
       "https://cf.bstatic.com/xdata/images/hotel/max1024x768/95421008.jpg?k=e4411cfc97d8ae2605f9fbe16e316753ac1f018bc7bf4eaf1190c6e563b1e023&o=&hp=1",
       "https://cf.bstatic.com/xdata/images/hotel/max1024x768/204905036.jpg?k=cf10ebeb0d4783ff46ff32e323331ee7ceb881946cad50a916f06ff880dddf8b&o=&hp=1"
      ],
-     "capacity": "300",
+     "capacity": 300,
      "description": "Hotel Ouarzazate is situated 1.2 km from Khandak Semmar Park, 1.3 km from Mohammed V Square and 2.1 km from Kasbah. It offers family rooms, a terrace, 24-hour reception, shuttle service, luggage storage and free Wi-Fi in all areas",
      "cityId": "city12",
      "userId": "adm4"
