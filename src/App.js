15788340124c8de25d2e2c29e06ae10dc7a115eb
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-11-03 20:23:47 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Main from "./layouts/Main";
+
 import Navbar from "./pages/Navbar";
 import {Routes,Route, Router} from 'react-router-dom';
 import Home1 from "./layouts/Home1";
 import CallToAction from "./pages/CallToAction";
-    /*< Main />*/
+  
+
 
 function App() {
   return (
