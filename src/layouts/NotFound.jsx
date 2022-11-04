8c564d3aa7ab5271e326c406cc96b8f197d7fdf8
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-04 17:00:11 -0300

    comienzo de page 404

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className='cuerpo-general'>
         <div className='c404'>
-            <h2>Upss...page not found</h2>
+            <h2>Upss....page not found</h2>
             <img  src="./img/f404.png" alt="error" />
         </div>
     </div>
