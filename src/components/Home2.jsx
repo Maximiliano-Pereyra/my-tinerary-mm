6e4ce909aaba62ce3ff96be7ec686c4b8e7c9f93
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-04 17:57:51 -0300

    correccion de carrusel y boton de scroll

--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import Logo from './Logo'
-import InicioU from './InicioU'
-import Empresa from './Empresa'
 import Carrusel from './Carrusel'
-import Footer from './Footer'
+
 
 export default function Home2() {
 
--- src/components/MapaFooter.jsx
+++ src/components/MapaFooter.jsx
@@ -5,7 +5,6 @@ export default function () {
     
     <div className='mapaFooter'>
     <h3>Acercate a planificar tus vacaiones!!!</h3>
-    
     <iframe class="map-img"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9287.856439170097!2d-58.43334912334138!3d-34.61040344712164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca429815227f%3A0x5d302e497c2cda87!2sR%C3%ADo%20de%20Janeiro%20300%2C%20C1405CCB%20CABA!5e0!3m2!1ses!2sar!4v1666755628652!5m2!1ses!2sar"
     ></iframe>
