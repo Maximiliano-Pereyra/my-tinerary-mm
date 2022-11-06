0ac2214cdcaa60a842355de3478ec188954312a2
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-06 15:15:04 -0300

    creacion de registro de hotels, pagina de hoteles y su input

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-
-import "./App.css";
-import Header from "./pages/Header";
-import { Routes, Route, Router } from "react-router-dom";
-import Home1 from "./pages/Home1";
-import CallToAction from "./pages/CallToAction";
-import Main from "./layouts/Main";
-import Home2 from "./components/Home2";
-import SignUp from "./pages/SignUp";
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <>
-    <Header></Header>
-    <div className="cuerpo-general">
-      <SignUp></SignUp>
-    </div>
-    <div className="footer">
-    <Footer></Footer>
-    </div>
-  </>
-  );
-
 import './App.css';
-import SignIn from './components/SignIn';
+import InputHotels from './components/InputHotels';
+import PginaDeCards from './components/PginasDeCards';
+
 
 function App() {
   return (
 <div className='cuerpo-general'>
-    <SignIn />
- </div>
+  <InputHotels/>
+    <PginaDeCards/>
+</div> 
   )
-
 }
 
 export default App;
